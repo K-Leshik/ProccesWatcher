82f1f1d5b23833e8177a8a4abedeef1b6172e2b3
K-Leshik <KA.Shindrich@yandex.ru>
2016-01-04 22:22:18 +0300

    First

---
+++ Owner/ComputerSelect.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Owner
+{
+    partial class frmComputerSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbComputers = new System.Windows.Forms.ComboBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbComputers
+            // 
+            this.cbComputers.FormattingEnabled = true;
+            this.cbComputers.Location = new System.Drawing.Point(20, 66);
+            this.cbComputers.Name = "cbComputers";
+            this.cbComputers.Size = new System.Drawing.Size(194, 21);
+            this.cbComputers.TabIndex = 0;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(58, 112);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(139, 112);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Выйти";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // lblText
+            // 
+            this.lblText.Location = new System.Drawing.Point(20, 26);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(194, 37);
+            this.lblText.TabIndex = 3;
+            this.lblText.Text = "Выберите компьютер, информацию о котором хотите посмотреть";
+            // 
+            // frmComputerSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 151);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.cbComputers);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmComputerSelect";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Выбор компьютера";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbComputers;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblText;
+
+    }
+}
+
---
+++ Owner/StartProgram.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Owner
+{
+    partial class StartProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatusText = new System.Windows.Forms.Label();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lblStatusText
+            // 
+            this.lblStatusText.Location = new System.Drawing.Point(12, 9);
+            this.lblStatusText.Name = "lblStatusText";
+            this.lblStatusText.Size = new System.Drawing.Size(338, 45);
+            this.lblStatusText.TabIndex = 0;
+            this.lblStatusText.Text = "Загрузка";
+            this.lblStatusText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Location = new System.Drawing.Point(12, 57);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(338, 23);
+            this.pbStatus.Step = 100;
+            this.pbStatus.TabIndex = 1;
+            // 
+            // StartProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 95);
+            this.Controls.Add(this.pbStatus);
+            this.Controls.Add(this.lblStatusText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StartProgram";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StartProgram";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatusText;
+        private System.Windows.Forms.ProgressBar pbStatus;
+    }
+}
